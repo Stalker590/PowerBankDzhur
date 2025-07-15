564803761c5ec37cd87ed783d57f11d531731d51
Stalker590 <sanyastalker1941@gmail.com>
2025-07-15 10:42:21 +0300

    Додано відповіді для бота

--- run.py
+++ run.py
@@ -1,1 +1,34 @@
-print("Хуйня")
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart,Command
+from aiogram.types import Message
+import asyncio
+import config
+import logging
+
+bot = Bot(config.TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(text='Привіт! Я Бот по продажі Павербанків по дешевій ціні по опту!')
+
+@dp.message(
+    F.text.in_([
+        "Купити", "Купити!", "купити", "buy",
+        "Купити Павербанк", "Купити Павербанк!", "купити Павербанк",
+        "buy Павербанк", "купить", "Купить"
+    ])
+)
+async def handle_buy(message: Message):
+    await message.answer("Ось список товарів:")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print('Bot stopped!')
